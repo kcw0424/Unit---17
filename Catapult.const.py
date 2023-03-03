c2c15190b266c22d52c0475f7f8ae004a15ff76d
kcw0424 <120763176+kcw0424@users.noreply.github.com>
2023-03-04 00:01:25 +0900

    Add files via upload

---
+++ Catapult.const.py
@@ -0,0 +1,19 @@
+g = 0.7 #중력가속도
+BASE_Y = 250 #투석기와 외계인 스프라이트가 위치할 Y좌표
+
+CATAPULT_READY = 0 #투석기 상태: 0(준비),1(발사)
+CATAPULT_FIRE = 1
+
+STONE_READY = 0 #돌상태: 0(준비), 1(비행중)
+STONE_FLY = 1
+
+MIN_POWER = 1# 발사파워
+MAX_POWER = 20
+
+MIN_DIRECTION = 20 # 발사각도
+MAX_DIRECTION = 85
+
+GAME_INIT = 0 #게임상태
+GAME_PLAY = 1
+GAME_CLEAR = 2
+GAME_OVER = 3
